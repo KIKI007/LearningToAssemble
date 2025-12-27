1d3e799861f5fd0ae684a8403dd2391a1c66af79
Ziqi Wang <qiqiustc@gmail.com>
2025-12-28 00:06:35 +0800

    Update simulator.py

--- src/learn2assemble/simulator.py
+++ src/learn2assemble/simulator.py
@@ -27,7 +27,7 @@ def init_ipm(parts: list[Trimesh],
                                   {
                                        "ipm_iter": 20,
                                        "pcg_iter": 100,
-                                       "conv_eps": 1E-3,
+                                       "conv_eps": 1E-2,
                                        "pcg_eps": 1E-9,
                                        "x_eps": 1E-7,
                                        "float_type": torch.float64,
